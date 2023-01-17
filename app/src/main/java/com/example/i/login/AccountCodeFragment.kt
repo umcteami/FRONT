d1a13e4394adf608f15d76830c94007b0c77bab3
youjin09222 <68536115+youjin09222@users.noreply.github.com>
2023-01-17 12:38:33 +0900

    [제리] LoginActivity UI 완성

---
+++ app/src/main/java/com/example/i/login/AccountCodeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.i.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import com.example.i.R
+
+class AccountCodeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_account_code, container, false)
+        val bt_ok = rootView.findViewById<ImageButton>(R.id.bt_ok)
+        val activity = activity as LoginActivity
+
+        // 이메일 확인
+        bt_ok.setOnClickListener{
+            activity.changeFragment(4)
+        }
+        return rootView
+    }
+}
