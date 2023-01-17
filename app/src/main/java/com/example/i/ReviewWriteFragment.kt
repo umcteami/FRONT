72d84eb0b3b4a060cfa32aade5b6f30bd4df4f25
ByungOok <rk97989@naver.com>
2023-01-18 02:10:56 +0900

    [Woogy] feat: 거래 후기 작성 페이지 추가

--- app/src/main/java/com/example/i/ReviewWriteFragment.kt
+++ app/src/main/java/com/example/i/ReviewWriteFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PostWrightFragment.newInstance] factory method to
+ * Use the [ReviewWriteFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PostWrightFragment : Fragment() {
+class ReviewWriteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class PostWrightFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_wright, container, false)
+        return inflater.inflate(R.layout.fragment_review_write, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class PostWrightFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PostWrightFragment.
+         * @return A new instance of fragment ReviewWriteFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PostWrightFragment().apply {
+            ReviewWriteFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
