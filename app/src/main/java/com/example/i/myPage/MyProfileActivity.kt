cad125cd665ef25ad8fc20412552fe5f0431de84
youjin09222 <68536115+youjin09222@users.noreply.github.com>
2023-01-22 01:00:18 +0900

    [제리] title -> Toolbar로 변경

--- app/src/main/java/com/example/i/myPage/MyProfileActivity.kt
+++ app/src/main/java/com/example/i/myPage/MyProfileActivity.kt
@@ -2,10 +2,33 @@ package com.example.i.myPage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.i.databinding.ActivityMyprofileBinding
 
 class MyProfileActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMyprofileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_myprofile)
+        viewBinding = ActivityMyprofileBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        setSupportActionBar(viewBinding.toolbar)
+        //Toolbar에 표시되는 제목의 표시 유무를 설정
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        //왼쪽 버튼 사용설정 (기본: 뒤로가기)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    //뒤로 가기 버튼 활성화
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
