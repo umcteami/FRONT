20d7007881d543ba146b720179da0896c89f7f7e
youjin09222 <68536115+youjin09222@users.noreply.github.com>
2023-01-21 21:24:01 +0900

    [제리] MyProfile UI

---
+++ app/src/main/java/com/example/i/myPage/FriendProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.i.myPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.i.R
+
+class FriendProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_profile)
+    }
+}
