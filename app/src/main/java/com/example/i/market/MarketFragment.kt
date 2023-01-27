e108d07c3590c79b672a537c68e80e82ff78b362
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-01-27 17:43:58 +0900

    [Summer] market 화면 수정

--- app/src/main/java/com/example/i/market/MarketFragment.kt
+++ app/src/main/java/com/example/i/market/MarketFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.i.databinding.FragmentMarketBinding
-import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayout
 
 class MarketFragment : Fragment() {
     private lateinit var viewBinding : FragmentMarketBinding
@@ -23,19 +23,42 @@ class MarketFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val pageVPAdapter = MarketVPAdapter(requireActivity())
-        viewBinding.vpMarket.adapter = pageVPAdapter
+//        viewBinding.tabMarket.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
 
-        val tabTitleArray = arrayOf(
-            "전체",
-            "맘마/까까",
-            "장난감",
-            "영양제/약/간호용품",
-            "기타"
-        )
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                val transaction = supportFragmentManager.beginTransaction()
+//                when(tab?.text) {
+//                    "전체" -> transaction.replace(viewBinding.tabMarket, MarketMainFragment() )
+//                    "맘마/까까" -> transaction.replace(viewBinding.tabMarket, SnackFragment())
+//                    "장난감" -> transaction.replace(viewBinding.tabMarket, ToyFragment())
+//                    "영양제/약/간호용품" -> transaction.replace(viewBinding.tabMarket, SupplementFragment())
+//                    "기타" -> transaction.replace(viewBinding.tabMarket, EtcFragment())
+//                }
+//                transaction.commit()
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//                TODO("Not yet implemented")
+//            }
+//        })
 
-        TabLayoutMediator(viewBinding.tapMarket, viewBinding.vpMarket) { tab, position ->
-            tab.text = tabTitleArray[position]
-        }.attach()
+//        val marketVPAdapter = MarketVPAdapter(requireActivity())
+//        viewBinding.vpMarket.adapter = marketVPAdapter
+//
+//        val tabTitleArray = arrayOf(
+//            "전체",
+//            "맘마/까까",
+//            "장난감",
+//            "영양제/약/간호용품",
+//            "기타"
+//        )
+//
+//        TabLayoutMediator(viewBinding.tapMarket, viewBinding.vpMarket) { tab, position ->
+//            tab.text = tabTitleArray[position]
+//        }.attach()
     }
 }
