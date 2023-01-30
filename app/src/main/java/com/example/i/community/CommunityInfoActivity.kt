a32a25bc6baa028ccd0763accbebf7a11b9605b8
ByungOok <rk97989@naver.com>
2023-01-31 00:26:27 +0900

    [Woogy] feat : 정보방/질문방/무지개/간호일기 추가

---
+++ app/src/main/java/com/example/i/community/CommunityInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.i.community
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.i.R
+
+class CommunityInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community_info)
+    }
+}
