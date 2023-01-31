d5d7881f5723f6d38cefc9022ddd97f818b37ae9
ByungOok <rk97989@naver.com>
2023-01-31 10:02:09 +0900

    [Woogy] feat: 이야기방 Recycler View 구현

--- app/src/main/java/com/example/i/community/BoardItem.kt
+++ app/src/main/java/com/example/i/community/BoardItem.kt
@@ -1,3 +1,11 @@
 package com.example.i.community
 
-data class BoardItem(val time: String, val title: String, val name: String)
+data class BoardItem(
+    val date: String,
+    val writer: String,
+    val title: String,
+    val room: String,
+    var view: String,
+    val heart: String,
+    var comment: String
+    )
