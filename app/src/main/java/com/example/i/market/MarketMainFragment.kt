0d77ee4650c75af6f13e47ada9566d995d9bf205
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-01-31 20:50:42 +0900

    [Summer] indicator 디자인 구현

--- app/src/main/java/com/example/i/market/MarketMainFragment.kt
+++ app/src/main/java/com/example/i/market/MarketMainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.i.market
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.example.i.R
 import com.example.i.databinding.FragmentMarketMainBinding
 
 class MarketMainFragment: Fragment() {
@@ -24,7 +28,9 @@ class MarketMainFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        var snapHelper = PagerSnapHelper()
         val mkpList: ArrayList<MarketP> = arrayListOf()
+        val indicator = IndicatorDecoration()
 
         mkpList.apply{
             add(MarketP("무료나눔", "강아지 껌", "2"))
@@ -49,8 +55,12 @@ class MarketMainFragment: Fragment() {
 
         viewBinding.rvPpl.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.HORIZONTAL, false)
         viewBinding.rvPpl.adapter = MarketPplRVAdapter(mkpList)
+        viewBinding.rvPpl.addItemDecoration(indicator)
+        snapHelper.attachToRecyclerView(viewBinding.rvPpl)
+
 
         val mkList: ArrayList<Market> = arrayListOf()
+        val customDecoration = CustomDecoration(2f, 2f, Color.GRAY)
 
         mkList.apply{
             add(Market("무료나눔", "강아지 껌", "7시간 전","조회 12", "2"))
@@ -65,5 +75,7 @@ class MarketMainFragment: Fragment() {
 
         viewBinding.rvMarket.layoutManager = LinearLayoutManager(context)
         viewBinding.rvMarket.adapter = MarketRVAdapter(mkList)
+        viewBinding.rvMarket.addItemDecoration(customDecoration)
+
     }
 }
