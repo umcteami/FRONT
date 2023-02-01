2789f7d2854f68ec954350e844edd1a32dfb4a27
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-02 05:58:48 +0900

    [Summer&Coco] 툴바 수정

--- app/src/main/java/com/example/i/signup/models/PostEmailInterface.kt
+++ app/src/main/java/com/example/i/signup/models/PostEmailInterface.kt
@@ -3,6 +3,6 @@ package com.example.i.signup.models
 interface PostEmailInterface {
 
     // 이메일 인증번호 발송
-    fun onPostEmailSuccess(response: EmailResponse)
+    fun onPostEmailSuccess(response: CodeResponse)
     fun onPostEmailFailure(message: String)
 }
