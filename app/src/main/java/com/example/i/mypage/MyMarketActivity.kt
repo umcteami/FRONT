916543be9aa2a4668b0ac9bc1fddb94444e33621
mnzy412 <mnzy412@gmail.com>
2023-02-01 23:37:30 +0900

    [코코] 마이페이지 화면 연결

--- app/src/main/java/com/example/i/mypage/MyMarketActivity.kt
+++ app/src/main/java/com/example/i/mypage/MyMarketActivity.kt
@@ -3,10 +3,18 @@ package com.example.i.mypage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.i.R
+import com.example.i.databinding.ActivityMyMarketBinding
 
 class MyMarketActivity : AppCompatActivity() {
+    lateinit var viewBinding: ActivityMyMarketBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_market)
+        viewBinding = ActivityMyMarketBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.backBtn.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/i/mypage/fragment/ReportFragment.kt
+++ app/src/main/java/com/example/i/mypage/fragment/ReportFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.i.R
+import com.example.i.databinding.ActivityMyMarketBinding
+import com.example.i.databinding.FragmentReportBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +20,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ReportFragment : Fragment() {
+    private lateinit var viewBinding: FragmentReportBinding
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,14 +32,31 @@ class ReportFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewBinding = FragmentReportBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false)
+        backFragment()
+
+        return viewBinding.root
+    }
+
+    //뒤로가기
+    private fun backFragment() {
+        viewBinding.backBtn.setOnClickListener {
+            activity?.let {
+                activity?.supportFragmentManager
+                    ?.beginTransaction()
+                    ?.remove(this)
+                    ?.commit()
+            }
+        }
     }
 
     companion object {
