cb16f58e7e23185c3b210cc1fe77cbb193cf2f39
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-02 00:36:37 +0900

    [Summer] 회원가입 전화번호, 인증번호 텍스트 입력 시 버튼 활성화 구현

--- app/src/main/java/com/example/i/signup/PhoneCodeFragment.kt
+++ app/src/main/java/com/example/i/signup/PhoneCodeFragment.kt
@@ -2,7 +2,10 @@ package com.example.i.signup
 
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -24,6 +27,41 @@ class PhoneCodeFragment : Fragment() {
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
         }
+
+        val activity = activity as SignupActivity
+
+        var message: String = ""
+
+        viewBinding.btOk.isEnabled = false
+
+        viewBinding.etPhonecode.addTextChangedListener(object: TextWatcher {
+            // 입력 전
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            // 입력 중
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                message = viewBinding.etPhonecode.text.toString()
+                viewBinding.btOk.isEnabled = message.isNotEmpty()
+
+                if (viewBinding.btOk.isEnabled == false) {
+                    viewBinding.btOk.setTextColor(Color.rgb(0x6B,0x66,0x66))
+                }
+                else {
+                    viewBinding.btOk.setTextColor(Color.WHITE)
+                }
+            }
+
+            // 입력 후
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+
+        viewBinding.btOk.setOnClickListener{
+            activity.changeFragment(4)
+        }
+
         return viewBinding.root
     }
 }
