aedce3a02e03075680a9f3e2825cd92d7a8c0c8d
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-04 23:51:45 +0900

    [Summer] 회원가입, 계정 찾기 UI 정리

--- app/src/main/java/com/example/i/login/NewPwFragment.kt
+++ app/src/main/java/com/example/i/login/NewPwFragment.kt
@@ -1,7 +1,10 @@
 package com.example.i.login
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,21 +12,79 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
 import com.example.i.R
+import com.example.i.databinding.FragmentNewPwBinding
 
 class NewPwFragment : Fragment() {
+
+    private lateinit var viewBinding: FragmentNewPwBinding
+    private var pw: String = ""
+    private var pwcode: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_new_pw, container, false)
-        val bt_ok = rootView.findViewById<Button>(R.id.bt_ok)
+        viewBinding = FragmentNewPwBinding.inflate(inflater, container, false)
+
+        viewBinding.btOk.isEnabled = false
+
+        viewBinding.etPw.addTextChangedListener(object: TextWatcher {
+            // 입력 전
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            // 입력 중
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                pw = viewBinding.etPw.text.toString()
+                pwcode = viewBinding.etPwcode.text.toString()
+
+                viewBinding.btOk.isEnabled = pw.isNotEmpty() && pwcode.isNotEmpty()
+
+                if (viewBinding.btOk.isEnabled == false) {
+                    viewBinding.btOk.setTextColor(Color.rgb(0x6B,0x66,0x66))
+                }
+                else {
+                    viewBinding.btOk.setTextColor(Color.WHITE)
+                }
+            }
+
+            // 입력 후
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+
+        viewBinding.etPwcode.addTextChangedListener(object: TextWatcher {
+            // 입력 전
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            // 입력 중
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                pw = viewBinding.etPw.text.toString()
+                pwcode = viewBinding.etPwcode.text.toString()
+
+                viewBinding.btOk.isEnabled = pw.isNotEmpty() && pwcode.isNotEmpty()
+
+                if (viewBinding.btOk.isEnabled == false) {
+                    viewBinding.btOk.setTextColor(Color.rgb(0x6B,0x66,0x66))
+                }
+                else {
+                    viewBinding.btOk.setTextColor(Color.WHITE)
+                }
+            }
+
+            // 입력 후
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
 
         // 로그인 화면으로 이동
-        bt_ok.setOnClickListener{
+        viewBinding.btOk.setOnClickListener{
             val intent = Intent(activity, LoginActivity::class.java)
             startActivity(intent)
         }
-        return rootView
+        return viewBinding.root
     }
 }
