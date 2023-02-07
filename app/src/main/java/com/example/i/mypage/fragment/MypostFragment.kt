51409ba88daf485a6ed2ae88d18fddedac55e811
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-07 19:32:43 +0900

    [Summer] 마이페이지 sort 버튼 구현

--- app/src/main/java/com/example/i/mypage/fragment/MypostFragment.kt
+++ app/src/main/java/com/example/i/mypage/fragment/MypostFragment.kt
@@ -1,21 +1,27 @@
 package com.example.i.mypage.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.i.Main2Activity
 import com.example.i.databinding.FragmentMypostBinding
+import com.example.i.market.MkFilterDialog
 import com.example.i.mypage.data.MyPost
 import com.example.i.mypage.data.PostRVAdapter
 
-class MypostFragment : Fragment() {
+class MypostFragment : Fragment(), View.OnClickListener {
     private lateinit var viewBinding: FragmentMypostBinding
+    private lateinit var main: Main2Activity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         viewBinding = FragmentMypostBinding.inflate(layoutInflater)
+
+        viewBinding.btSort.setOnClickListener(this)
 
         backFragment() // 뒤로가기
 
@@ -27,30 +33,49 @@ class MypostFragment : Fragment() {
 
         val PostList: ArrayList<MyPost> = arrayListOf()
 
-        PostList.apply{
-            add(MyPost("자유방", "다니고 계시는 병원 정보 좀 부탁드려요 (서울/경기도)", "22.12.18","조회 12", "2"))
-            add(MyPost("질문방", "해피가 이런 증상을 보이는데 괜찮은 건가요?", "7시간 전","조회 12", "2"))
-            add(MyPost("정보방", "소동물 전문 병원 정보 모음 (지역별, 2022.12월 업데이트)", "7시간 전","조회 12", "2"))
-            add(MyPost("자유방", "다니고 계시는 병원 정보 좀 부탁드려요 (서울/경기도)", "22.12.18","조회 12", "2"))
-            add(MyPost("질문방", "해피가 이런 증상을 보이는데 괜찮은 건가요?", "7시간 전","조회 12", "2"))
-            add(MyPost("정보방", "소동물 전문 병원 정보 모음 (지역별, 2022.12월 업데이트)", "7시간 전","조회 12", "2"))
-            add(MyPost("자유방", "다니고 계시는 병원 정보 좀 부탁드려요 (서울/경기도)", "22.12.18","조회 12", "2"))
-            add(MyPost("질문방", "해피가 이런 증상을 보이는데 괜찮은 건가요?", "7시간 전","조회 12", "2"))
-            add(MyPost("정보방", "소동물 전문 병원 정보 모음 (지역별, 2022.12월 업데이트)", "7시간 전","조회 12", "2"))
+        PostList.apply {
+            add(MyPost("자유방", "다니고 계시는 병원 정보 좀 부탁드려요 (서울/경기도)", "22.12.18", "조회 12", "2"))
+            add(MyPost("질문방", "해피가 이런 증상을 보이는데 괜찮은 건가요?", "7시간 전", "조회 12", "2"))
+            add(MyPost("정보방", "소동물 전문 병원 정보 모음 (지역별, 2022.12월 업데이트)", "7시간 전", "조회 12", "2"))
+            add(MyPost("자유방", "다니고 계시는 병원 정보 좀 부탁드려요 (서울/경기도)", "22.12.18", "조회 12", "2"))
+            add(MyPost("질문방", "해피가 이런 증상을 보이는데 괜찮은 건가요?", "7시간 전", "조회 12", "2"))
+            add(MyPost("정보방", "소동물 전문 병원 정보 모음 (지역별, 2022.12월 업데이트)", "7시간 전", "조회 12", "2"))
+            add(MyPost("자유방", "다니고 계시는 병원 정보 좀 부탁드려요 (서울/경기도)", "22.12.18", "조회 12", "2"))
+            add(MyPost("질문방", "해피가 이런 증상을 보이는데 괜찮은 건가요?", "7시간 전", "조회 12", "2"))
+            add(MyPost("정보방", "소동물 전문 병원 정보 모음 (지역별, 2022.12월 업데이트)", "7시간 전", "조회 12", "2"))
         }
 
         viewBinding.recyclerview.layoutManager = LinearLayoutManager(context)
         viewBinding.recyclerview.adapter = PostRVAdapter(PostList)
+
     }
 
     //뒤로가기
     private fun backFragment() {
         viewBinding.backBtn.setOnClickListener {
-            activity?.let{
+            activity?.let {
                 activity?.supportFragmentManager
                     ?.beginTransaction()
                     ?.remove(this)
                     ?.commit()
+            }
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        main = context as Main2Activity
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            viewBinding.btSort.id -> {
+                val dlg = MkFilterDialog(main)
+                dlg.setOnOkClickedListener { content ->
+                    viewBinding.btSort.text = content
+                }
+                dlg.show()
             }
         }
     }
