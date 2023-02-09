dad16699df82ff26490ebeefb326b5a343c7f7eb
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-10 02:21:31 +0900

    [Summer] home 리사이클러뷰 클릭 이벤트 구현

--- app/src/main/java/com/example/i/home/TtlRVAdapter.kt
+++ app/src/main/java/com/example/i/home/TtlRVAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.i.home
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.i.databinding.ListItemTtl2Binding
@@ -8,11 +9,12 @@ import com.example.i.databinding.ListItemTtlBinding
 import com.example.i.home.Const.HASIMAGE
 import com.example.i.home.Const.NOIMAGE
 
-
 class TtlRVAdapter(private val ttlList:ArrayList<Ttls>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    inner class TtlWithImageViewHolder(private val viewBinding: ListItemTtlBinding) :
-    RecyclerView.ViewHolder(viewBinding.root) {
+    var itemClick: TtlRVAdapter.ItemClick? = null
+
+    inner class TtlWithImageViewHolder(val viewBinding: ListItemTtlBinding) :
+        RecyclerView.ViewHolder(viewBinding.root) {
         fun bind(ttls: Ttls) {
             viewBinding.ttlTitle.text = ttls.title
             viewBinding.ttlPicture.setImageResource(ttls.picture!!)
@@ -24,15 +26,15 @@ class TtlRVAdapter(private val ttlList:ArrayList<Ttls>) : RecyclerView.Adapter<R
         }
     }
 
-    inner class TtlWithoutImageViewHolder(private val viewBinding: ListItemTtl2Binding) :
-        RecyclerView.ViewHolder(viewBinding.root) {
+    inner class TtlWithoutImageViewHolder(val viewBinding2: ListItemTtl2Binding) :
+        RecyclerView.ViewHolder(viewBinding2.root) {
         fun bind(ttls: Ttls) {
-            viewBinding.ttlTitle.text = ttls.title
-            viewBinding.ttlWriter.text = ttls.writer
-            viewBinding.ttlDate.text = ttls.date
-            viewBinding.ttlViewNum.text = ttls.view
-            viewBinding.ttlHaertNum.text = ttls.heart
-            viewBinding.tlCommentNum.text = ttls.comment
+            viewBinding2.ttlTitle.text = ttls.title
+            viewBinding2.ttlWriter.text = ttls.writer
+            viewBinding2.ttlDate.text = ttls.date
+            viewBinding2.ttlViewNum.text = ttls.view
+            viewBinding2.ttlHaertNum.text = ttls.heart
+            viewBinding2.tlCommentNum.text = ttls.comment
         }
     }
 
@@ -43,7 +45,7 @@ class TtlRVAdapter(private val ttlList:ArrayList<Ttls>) : RecyclerView.Adapter<R
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return if (viewType == HASIMAGE) {
-           val view = ListItemTtlBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+            val view = ListItemTtlBinding.inflate(LayoutInflater.from(parent.context),parent, false)
             TtlWithImageViewHolder(view)
         } else {
             val view = ListItemTtl2Binding.inflate(LayoutInflater.from(parent.context),parent,false)
@@ -53,14 +55,32 @@ class TtlRVAdapter(private val ttlList:ArrayList<Ttls>) : RecyclerView.Adapter<R
 
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-       if (getItemViewType(position) == HASIMAGE) {
-           (holder as TtlWithImageViewHolder).bind(ttlList[position])
-       }else {
-           (holder as TtlWithoutImageViewHolder).bind(ttlList[position])
-       }
+        if (getItemViewType(position) == HASIMAGE) {
+            (holder as TtlWithImageViewHolder).bind(ttlList[position])
+        }else {
+            (holder as TtlWithoutImageViewHolder).bind(ttlList[position])
+        }
+
+        if (itemClick != null) {
+            if (ttlList[position].hasImage == HasImage.TRUE) {
+                (holder as TtlRVAdapter.TtlWithImageViewHolder).viewBinding!!.itemTtl.setOnClickListener(
+                    View.OnClickListener {
+                        itemClick?.onClick(it, position)
+                    })
+            }  else {
+                (holder as TtlRVAdapter.TtlWithoutImageViewHolder).viewBinding2!!.itemTtl2.setOnClickListener(
+                    View.OnClickListener {
+                        itemClick?.onClick(it, position)
+                    })
+            }
+        }
     }
 
     override fun getItemCount(): Int = ttlList.size
+
+    interface ItemClick {
+        fun onClick(view: View, position: Int)
+    }
 
 }
 
