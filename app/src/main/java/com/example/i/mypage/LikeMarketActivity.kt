3d4e884aea9b69fa50a193aaf3e0fe69abb079bb
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-10 02:56:21 +0900

    [Summer] mypage 리사이클러뷰 클릭 이벤트 구현2

--- app/src/main/java/com/example/i/mypage/LikeMarketActivity.kt
+++ app/src/main/java/com/example/i/mypage/LikeMarketActivity.kt
@@ -1,9 +1,13 @@
 package com.example.i.mypage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.i.community.talk.CommunityPostActivity
 import com.example.i.databinding.ActivityLikeMarketBinding
+import com.example.i.market.MarketPostActivity
 import com.example.i.mypage.data.LikeMarket
 import com.example.i.mypage.data.LikeMarketRVAdapter
 
@@ -15,6 +19,7 @@ class LikeMarketActivity : AppCompatActivity() {
         viewBinding = ActivityLikeMarketBinding.inflate(layoutInflater)
 
         val LikeList: ArrayList<LikeMarket> = arrayListOf()
+        val adapter = LikeMarketRVAdapter(LikeList)
 
         LikeList.apply{
             add(LikeMarket("3000원", "어쩌구저쩌구 사료", "10시간 전","조회 12", "2"))
@@ -27,7 +32,14 @@ class LikeMarketActivity : AppCompatActivity() {
         }
 
         viewBinding.recyclerview.layoutManager = LinearLayoutManager(this)
-        viewBinding.recyclerview.adapter = LikeMarketRVAdapter(LikeList)
+        viewBinding.recyclerview.adapter = adapter
+
+        adapter!!.itemClick = object : LikeMarketRVAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val intent = Intent(this@LikeMarketActivity, MarketPostActivity::class.java)
+                startActivity(intent)
+            }
+        }
 
         viewBinding.backBtn.setOnClickListener {
             finish()
