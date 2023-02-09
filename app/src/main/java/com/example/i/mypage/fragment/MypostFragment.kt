3961121ce44b7625d602895acdd40a0f0d0f70a6
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-10 02:47:18 +0900

    [Summer] mypage 리사이클러뷰 클릭 이벤트 구현

--- app/src/main/java/com/example/i/mypage/fragment/MypostFragment.kt
+++ app/src/main/java/com/example/i/mypage/fragment/MypostFragment.kt
@@ -1,11 +1,13 @@
 package com.example.i.mypage.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.i.Main2Activity
+import com.example.i.community.talk.CommunityPostActivity
 import com.example.i.databinding.FragmentMypostBinding
 import com.example.i.market.customdialog.MkFilterDialog
 import com.example.i.mypage.data.MyPost
@@ -32,6 +34,7 @@ class MypostFragment : Fragment(), View.OnClickListener {
         super.onViewCreated(view, savedInstanceState)
 
         val PostList: ArrayList<MyPost> = arrayListOf()
+        val adapter = PostRVAdapter(PostList)
 
         PostList.apply {
             add(MyPost("자유방", "다니고 계시는 병원 정보 좀 부탁드려요 (서울/경기도)", "22.12.18", "조회 12", "2"))
@@ -46,7 +49,14 @@ class MypostFragment : Fragment(), View.OnClickListener {
         }
 
         viewBinding.recyclerview.layoutManager = LinearLayoutManager(context)
-        viewBinding.recyclerview.adapter = PostRVAdapter(PostList)
+        viewBinding.recyclerview.adapter = adapter
+
+        adapter!!.itemClick = object : PostRVAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val intent = Intent(requireActivity(), CommunityPostActivity::class.java)
+                startActivity(intent)
+            }
+        }
 
     }
 
