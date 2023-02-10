2d220dda5902a1a0ed6e960282bff7538c9cb3d3
youjin09222 <68536115+youjin09222@users.noreply.github.com>
2023-02-10 17:50:13 +0900

    [제리] 탈퇴하기 API 연결

--- app/src/main/java/com/example/i/mypage/RevokeActivity.kt
+++ app/src/main/java/com/example/i/mypage/RevokeActivity.kt
@@ -2,10 +2,14 @@ package com.example.i.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.i.config.BaseResponse
 import com.example.i.databinding.ActivityRevokeBinding
 import com.example.i.mypage.customdialog.PopupRevokeDialog
+import com.example.i.mypage.data.*
 
-class RevokeActivity : AppCompatActivity() {
+class RevokeActivity : AppCompatActivity(), RevokeInterface {
     lateinit var viewBinding: ActivityRevokeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +30,20 @@ class RevokeActivity : AppCompatActivity() {
         viewBinding.btCall.setOnClickListener {
             val dialog = PopupRevokeDialog()
             dialog.show(supportFragmentManager, "custom dialog")
-        }
 
+            // 서버에 값 보냄
+            // val postRequest = PostRevokeRequest(33)
+            RevokeService(this).tryPostRevoke(33)
+        }
+    }
+    // 탈퇴하기 API
+    override fun onPostRevokeSuccess(response: BaseResponse) {
+        // Result message
+        response.message?.let { Toast.makeText(this, it, Toast.LENGTH_SHORT).show() }
+    }
+
+    // 서버 연결 실패
+    override fun onPostRevokeFailure(message: String) {
+        Log.d("error", "오류 : $message")
     }
 }
