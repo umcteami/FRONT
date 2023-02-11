281d376e85252739c6deee7c4a8c333dc6c937cf
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-11 13:14:53 +0900

    [Summer] 채팅목록 adapter 수정3

--- app/src/main/java/com/example/i/chat/ChatRVAdpater.kt
+++ app/src/main/java/com/example/i/chat/ChatRVAdpater.kt
@@ -1,4 +1,41 @@
 package com.example.i.chat
 
-class ChatRVAdpater {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.i.databinding.ItemMeChatBinding
+import com.example.i.databinding.ItemYouChatBinding
+
+class ChatRVAdpater(private val meList: ArrayList<ChatMe>, private val youList: ArrayList<ChatYou>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    inner class meHolder(val viewBinding: ItemMeChatBinding): RecyclerView.ViewHolder(viewBinding.root) {
+        fun bind(me: ChatMe) {
+            viewBinding.tvMessage.text = me.content
+            viewBinding.tvDate.text = me.time
+            if (me.Check) {
+                viewBinding.ivShow.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    inner class youHolder(val viewBinding: ItemYouChatBinding): RecyclerView.ViewHolder(viewBinding.root) {
+        fun bind(you: ChatYou) {
+            viewBinding.tvMessage.text = you.content
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
 }
