1bc9ea659c8295dbf6552164df91804d56ffc503
mnzy412 <mnzy412@gmail.com>
2023-02-12 16:50:48 +0900

    [코코] 이야기방 오류 수정

--- app/src/main/java/com/example/i/community/talk/CommunityTalkroomFragment.kt
+++ app/src/main/java/com/example/i/community/talk/CommunityTalkroomFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.i.community.BoardItem
@@ -18,6 +19,9 @@ import com.example.i.home.HasImage
 
 class CommunityTalkroomFragment : Fragment(), TalkroomInterface {
     private lateinit var viewBinding: FragmentTalkroomBinding
+    val itemList: ArrayList<BoardItem> = arrayListOf()
+    var hasImage: HasImage = HasImage.TRUE
+    val Tadapter = CommunityBoardAdapter(itemList)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +30,15 @@ class CommunityTalkroomFragment : Fragment(), TalkroomInterface {
 
         viewBinding = FragmentTalkroomBinding.inflate(layoutInflater)
 
-        //리사이클러뷰 데이터 추가
-        TalkroomService(this).tryGetTalkroom(5)
+        //리사이클러뷰 데이터 추가=
+        TalkroomService(this).tryGetTalkroom()
+
+        Tadapter!!.itemClick = object : CommunityBoardAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val intent = Intent(requireActivity(), CommunityPostActivity::class.java)
+                startActivity(intent)
+            }
+        }
 
         return viewBinding.root
 
@@ -35,46 +46,46 @@ class CommunityTalkroomFragment : Fragment(), TalkroomInterface {
 
     override fun onGetTalkroomSuccess(response: TalkroomResponse) {
         if (response.isSuccess) {
-            val itemList: ArrayList<BoardItem> = arrayListOf()
-            val hasImage: HasImage
-            val Tadapter = CommunityBoardAdapter(itemList)
 
-            if (response.result[6] != null) {
-                hasImage = HasImage.TRUE
-            } else {
-                hasImage = HasImage.FALSE
-            }
+            val index: Int = response.result.size - 1
 
-            itemList.apply {
-                add(
-                    BoardItem(
-                        hasImage, //이미지 있는지 여부
-                        response.result[1].toString(), //세부 카테고리(1.수다방, 2.질문방, 3.정보방)
-                        response.result[5].toString(), //제목
-                        response.result[6].toString(), //게시글 이미지
-                        response.result[4].toString(), //작성자 닉네임
-                        response.result[10].toString(), //작성일자 및 시간
-                        response.result[7].toString(), //조회수
-                        response.result[9].toString(), //하트수
-                        response.result[8].toString() //댓글수
+            for (i in 0..index) {
+
+                if (response.result[i].img != null) {
+                    hasImage = HasImage.TRUE
+                } else {
+                    hasImage = HasImage.FALSE
+                }
+                itemList.apply {
+                    add(
+                        BoardItem(
+                            hasImage, //이미지 있는지 여부
+                            response.result[i].roomType.toString(), //세부 카테고리(1.수다방, 2.질문방, 3.정보방)
+                            response.result[i].title, //제목
+                            response.result[i].img, //게시글 이미지
+                            response.result[i].memNick, //작성자 닉네임
+                            response.result[i].createAt, //작성일자 및 시간
+                            response.result[i].hit.toString(), //조회수
+                            response.result[i].likeCnt.toString(), //하트수
+                            response.result[i].commentCnt.toString() //댓글수
+                        )
                     )
-                )
-                //
-            }
 
-            viewBinding.rvBoard.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-            viewBinding.rvBoard.adapter = Tadapter
-
-            Tadapter!!.itemClick = object : CommunityBoardAdapter.ItemClick {
-                override fun onClick(view: View, position: Int) {
-                    val intent = Intent(requireActivity(), CommunityPostActivity::class.java)
-                    startActivity(intent)
                 }
             }
+
+            viewBinding.rvBoard.layoutManager =
+                LinearLayoutManager(requireActivity())
+            viewBinding.rvBoard.adapter = Tadapter
+
+            Toast.makeText(activity, response.message, Toast.LENGTH_SHORT).show()
         }
     }
 
+
     override fun onGetTalkroomFailure(message: String) {
-        Log.d("error","카테고리 이야기방 전체 오류: $message")
+        Log.d("error", "카테고리 이야기방 전체 오류: $message")
     }
 }
+
+
--- app/src/main/java/com/example/i/community/talk/models/talkroom/TalkroomRetrofitInterface.kt
+++ app/src/main/java/com/example/i/community/talk/models/talkroom/TalkroomRetrofitInterface.kt
@@ -7,7 +7,5 @@ import retrofit2.http.Query
 interface TalkroomRetrofitInterface {
 
     @GET("/feeds/story/all")
-    fun getTalkroom(
-        @Query("page") page: Int
-    ): Call<TalkroomResponse>
+    fun getTalkroom(): Call<TalkroomResponse>
 }
