9e48466b37ab6b185b4303f02ec6e0c3738ab0cf
mnzy412 <mnzy412@gmail.com>
2023-02-12 11:30:23 +0900

    [코코] ttl 어댑터 오류 해결

--- app/src/main/java/com/example/i/home/model/TtlListService.kt
+++ app/src/main/java/com/example/i/home/model/TtlListService.kt
@@ -14,6 +14,7 @@ class TtlListService(val TtlListInterface: TtlListInterface){
                 (response.body() as TtlListResponse?)?.let {
                     TtlListInterface.onGetTtlListSuccess(
                         it
+
                     )
                 }
             }
