147d61f28820b05e4e3254f0aba7fa736c3ebe79
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-12 15:13:58 +0900

    [Summer] 수정

--- app/src/main/java/com/example/i/chat/MessageRVAdapter.kt
+++ app/src/main/java/com/example/i/chat/MessageRVAdapter.kt
@@ -23,7 +23,7 @@ class MessageRVAdapter(private val mList: ArrayList<Message>): RecyclerView.Adap
             viewBinding.tvNickname.text = Message.nickname
             viewBinding.tvChat.text = Message.chat
             viewBinding.tvTime.text = Message.time
-            if (Message.num?.isEmpty() == true || Message.num!!.equals(0)){
+            if (Message.num?.isEmpty() == true || Message.num!!.equals("0")){
                 viewBinding.tvNum.visibility = View.INVISIBLE
             }
             else {
