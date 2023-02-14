aed097a05cc63491a6098c7b96b1f5bf21348157
Lee-Song-Ha <songha1744@swu.ac.kr>
2023-02-14 21:05:43 +0900

    [Summer] ..

--- app/src/main/java/com/example/i/chat/customdialog/MessageNoticeDialog.kt
+++ app/src/main/java/com/example/i/chat/customdialog/MessageNoticeDialog.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import com.example.i.chat.MessageListFragment
 import com.example.i.chat.model.ChatDeleteInterface
 import com.example.i.chat.model.ChatDeleteRequest
 import com.example.i.chat.model.ChatDeleteService
