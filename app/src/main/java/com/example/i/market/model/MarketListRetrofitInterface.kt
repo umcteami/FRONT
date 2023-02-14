ffcd4fb7974df6b32842bb984194c89938ab0100
mnzy412 <mnzy412@gmail.com>
2023-02-15 02:20:55 +0900

    [코코] 나눔장터 오류 확인

--- app/src/main/java/com/example/i/market/model/MarketListRetrofitInterface.kt
+++ app/src/main/java/com/example/i/market/model/MarketListRetrofitInterface.kt
@@ -1,5 +1,6 @@
 package com.example.i.market.model
 
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,7 +9,7 @@ interface MarketListRetrofitInterface {
 
     @GET("/market/latest")
     fun getMarketList(
-        @Query ("category") category: String,
-        @Query ("soldout") soldout: String
-    ): retrofit2.Call<MarketListResponse>
+        @Query ("category") category: String? = null,
+        @Query ("soldout") soldout: String? = null
+    ): Call<MarketListResponse>
 }
