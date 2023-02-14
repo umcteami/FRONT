738ec07ec6bd1350a1030acf80e4534d0ed94452
mnzy412 <mnzy412@gmail.com>
2023-02-15 04:10:26 +0900

    [코코] 나눔장터 오류 확인

--- app/src/main/java/com/example/i/market/MarketMainFragment.kt
+++ app/src/main/java/com/example/i/market/MarketMainFragment.kt
@@ -1,8 +1,10 @@
 package com.example.i.market
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,26 +12,25 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
+import com.example.i.Main2Activity
 import com.example.i.databinding.FragmentMarketMainBinding
+import com.example.i.login.memIdx
+import com.example.i.market.model.MarketListInterface
+import com.example.i.market.model.MarketListResponse
+import com.example.i.market.model.MarketListService
 import com.google.android.material.tabs.TabLayoutMediator
 
-class MarketMainFragment: Fragment() {
+class MarketMainFragment: Fragment(), MarketListInterface {
     private lateinit var viewBinding: FragmentMarketMainBinding
-
-    val mkList: ArrayList<Market> = arrayListOf()
-    val adapter = MarketRVAdapter(mkList, requireActivity())
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewBinding = FragmentMarketMainBinding.inflate(layoutInflater)
+        viewBinding = FragmentMarketMainBinding.inflate(inflater, container, false)
 
-        mkList.apply{
-            add(Market("0", "강아지 껌", 30000,"2022-12-23-", 5,6,null))
-            add(Market("1", "강아지 장난감", 30000,"2022-12-23-", 5,6,null))
-        }
+        MarketListService(this).tryGetMarketList(memIdx)
 
         return viewBinding.root
     }
@@ -40,24 +41,55 @@ class MarketMainFragment: Fragment() {
         val marketVPAdapter = MarketVPAdapter(requireActivity())
         viewBinding.vpPpl.adapter = marketVPAdapter
 
+
         TabLayoutMediator(viewBinding.dotsLayout!!, viewBinding.vpPpl!!){tab, position ->
             viewBinding.vpPpl.setCurrentItem(tab.position)
         }.attach()
 
+    }
 
-        val customDecoration = CustomDecoration(2f, 2f, Color.rgb(0xB4,0xB4,0xB4))
+    override fun onGetMarketListSuccess(response: MarketListResponse) {
+        if (response.isSuccess) {
+
+            val index:Int = 0
+
+            val customDecoration = CustomDecoration(2f, 2f, Color.rgb(0xB4,0xB4,0xB4))
+            val mkList: ArrayList<Market> = arrayListOf()
+            val adapter = MarketRVAdapter(mkList, requireActivity())
+            for (i in 0..index) {
 
 
-        viewBinding.rvMarket.layoutManager = LinearLayoutManager(context)
-        viewBinding.rvMarket.adapter = adapter
-        viewBinding.rvMarket.addItemDecoration(customDecoration)
+                mkList.apply {
+                    add(
+                        Market(
+                            response.result[i].soldout,
+                            response.result[i].title,
+                            response.result[i].price,
+                            response.result[i].createAt,
+                            response.result[i].hit,
+                            response.result[i].likeCnt,
+                            response.result[i].img
 
-        adapter!!.itemClick = object : MarketRVAdapter.ItemClick {
+                        )
+                    )
+                }
+            }
 
-            override fun onClick(view: View, position: Int) {
-                val intent = Intent(activity, MarketPostActivity::class.java)
-                startActivity(intent)
+            viewBinding.rvMarket.layoutManager = LinearLayoutManager(activity)
+            viewBinding.rvMarket.adapter = adapter
+            viewBinding.rvMarket.addItemDecoration(customDecoration)
+
+            adapter!!.itemClick = object : MarketRVAdapter.ItemClick {
+
+                override fun onClick(view: View, position: Int) {
+                    val intent = Intent(activity, MarketPostActivity::class.java)
+                    startActivity(intent)
+                }
             }
         }
+    }
+
+    override fun onGetMarketListFailure(message: String) {
+        Log.d("error", "오류: $message")
     }
 }
